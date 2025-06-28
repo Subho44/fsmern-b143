29a5b942d3e4b85c91da98cc10620532bff98275
Subho44 <98902921+Subho44@users.noreply.github.com>
2025-06-28 11:57:48 +0530

    updated
    done

--- RG-FSMERN-B143/fsmern-b143-main/RG-FSMERN-B143/express-server/crud-operation/routes/userRoutes.js
+++ RG-FSMERN-B143/fsmern-b143-main/RG-FSMERN-B143/express-server/crud-operation/routes/userRoutes.js
@@ -16,4 +16,34 @@ router.get('/',async(req,res)=>{
     const users = await User.find();
     res.json(users);
 });
+//read one data
+router.get('/:id',async(req,res)=>{
+    try {
+        const user = await User.findById(req.params.id);
+        if(!user) return res.status(404).json({error:"user not found"});
+        res.json(user);
+    } catch(err) {
+       res.status(404).json({error:err.message}); 
+    }
+});
+//update
+router.put('/:id',async(req,res)=>{
+    try {
+        const updateuser = await User.findByIdAndUpdate(req.params.id,req.body,{new:true});
+        if(!updateuser) return res.status(404).json({error:"user not found"});
+        res.json(updateuser);
+    } catch(err) {
+       res.status(404).json({error:err.message}); 
+    }
+});
+//delete
+router.delete('/:id',async(req,res)=>{
+    try {
+        const deleteuser = await User.findByIdAndDelete(req.params.id);
+        if(!deleteuser) return res.status(404).json({error:"user not found"});
+        res.json(deleteuser);
+    } catch(err) {
+       res.status(404).json({error:err.message}); 
+    }
+});
 module.exports = router;
